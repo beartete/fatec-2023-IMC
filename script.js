ec55c009b8e33006cc38b8d9c7f14c27b1294123
beartete <isabellacristinavieira2003@gmail.com>
2023-03-10 08:53:22 -0300

    imc

--- script.js
+++ script.js
@@ -22,15 +22,15 @@ function calcularIMC() {
   // Verificando a classificação do IMC e exibindo uma mensagem
   if (imc < 18.5) {
     resultadoInput.value += " (Abaixo do peso)";
-  } else if (imc >= 18.6 && imc < 24.9) {
+  } else if (imc >= 18.6 && imc <= 24.9) {
     resultadoInput.value += " (Peso normal)";
-  } else if (imc >= 25 && imc < 29.9) {
+  } else if (imc >= 25 && imc <= 29.9) {
     resultadoInput.value += " (Sobrepeso)";
-  } else if (imc >= 30 && imc < 34.9) {
+  } else if (imc >= 30 && imc <= 34.9) {
     resultadoInput.value += " (Obesidade grau 1)";
-  } else if (imc >= 35 && imc < 39.9) {
+  } else if (imc >= 35 && imc <= 39.9) {
     resultadoInput.value += " (Obesidade grau 2)";
-  } else {
+  } else if (imc >= 40){
     resultadoInput.value += " (Obesidade grau 3)";
   }
 }
